9b2cf999095b0ae4119a715862de13b14be7d408
cczyWyc <274230928@qq.com>
2022-11-28 00:50:40 +0800

    crawler: set html encoding and use xpath and selector get a card

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module crawler-geektime
 
 go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,61 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
 	"net/http"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
+	body, err := Fetch(url)
+	if err != nil {
+		fmt.Printf("read contect faield: %v", err)
+		return
+	}
+
+	// load html
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil {
+		fmt.Printf("htmlquery parse failed: %v", err)
+	}
+	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		fmt.Printf("Review %d: %s", i, title)
+	})
+}
+
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error: %v", err)
-		return
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error status code: %v", err)
-		return
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	htmlEncoding := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, htmlEncoding.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
 
-	body, err := io.ReadAll(resp.Body)
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("read contect failed: %v", err)
+		fmt.Printf("fetch error: %v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
