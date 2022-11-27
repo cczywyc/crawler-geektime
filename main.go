437ef284acfde7f64167b1f6030023a32337372e
cczyWyc <274230928@qq.com>
2022-11-27 23:29:38 +0800

    crawler: print resp

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.thepaper.cn/"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("fetch url error: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code: %v", err)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read contect failed: %v", err)
+	}
+
+	fmt.Println("body:", string(body))
+}
