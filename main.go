29e55cf405a4775d3f54cfbd33fa4e7115392140
cczyWyc <274230928@qq.com>
2022-11-29 21:00:19 +0800

    crawler-geektime: add fetch interface, and Simulated browser access

--- main.go
+++ main.go
@@ -1,61 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"crawler-geektime/collect"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io"
-	"net/http"
+	"time"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.FetCher = collect.BrowserFetch{
+		Timeout: 3000 * time.Millisecond,
+	}
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Printf("read contect faield: %v", err)
 		return
 	}
+	fmt.Println(body)
 
-	// load html
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
 	if err != nil {
-		fmt.Printf("htmlquery parse failed: %v", err)
+		fmt.Printf("read content failed: %v", err)
 	}
 	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
 		title := s.Text()
-		fmt.Printf("Review %d: %s", i, title)
+		fmt.Printf("Review %d %s\n", i, title)
 	})
-}
-
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code: %v", err)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	htmlEncoding := DetermineEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, htmlEncoding.NewDecoder())
-	return io.ReadAll(utf8Reader)
-}
-
-func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		fmt.Printf("fetch error: %v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
